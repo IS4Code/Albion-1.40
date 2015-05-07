8762b7f36a52e1e4fcb3b1a4ec287e18e651d982
IllidanS4 <illidans4@gmail.com>
2015-05-07 17:37:01 +0200

    Patch cleans itself

--- patch.bat
+++ patch.bat
@@ -1,9 +1,13 @@
 @echo off
 for /r XLDLIBS %%i in (*.xlp, *.xlz, *.xlp.gz) do (
   echo %%~nxi
-  albtool -i -f xlp %%i %%~dpni.XLD
+  albtool.exe -i -f xlp "%%i" "%%~dpni.XLD"
+  del "%%i"
 )
 for /r XLDLIBS %%i in (*.pat, *.pat.gz) do (
   echo %%~nxi
-  albtool -c -f xlp %%i %%~dpni.DAT
+  albtool.exe -c -f xlp "%%i" "%%~dpni.DAT"
+  del "%%i"
 )
+del albtool.exe
+del patch.bat
